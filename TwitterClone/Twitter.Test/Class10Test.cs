71d2217c04b85f4da2bbec586bf9e6d7127051d2
Iftekar Islam <iftekharislam923@gmail.com>
2026-09-07 16:13:17 +0600

    apply polymorphism & interface

---
+++ TwitterClone/Twitter.Test/Class10Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitterClone.Domain.Entities;
+
+namespace Twitter.Test
+{
+    public class Class10Test
+    {
+        public void Run()
+        {
+            Tweet likeableTweet = new Tweet("This is a another tweet!");
+            Console.WriteLine(likeableTweet.CanBeLiked());
+            var maxTweetLength = 200;
+        }
+    }
+}
