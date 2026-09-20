fa0d40380c2195e1034b4de648140c5262ed91e8
Iftekar Islam <iftekharislam923@gmail.com>
2026-09-21 00:18:38 +0600

    Add Twitter & User Controller

---
+++ TwitterClone/TwitterClone.Api/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TwitterClone.Domain.Entities;
+
+namespace TwitterClone.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = new List<User>
+            {
+                new User(
+                    Guid.NewGuid(),
+                    "john.doe@example.com",
+                    "John",
+                    "Doe",
+                    DateTime.UtcNow,
+                    Guid.NewGuid()
+                ),
+                new User(
+                    Guid.NewGuid(),
+                    "janesmith@gmail.com",
+                    "Jane",
+                    "Smith",
+                    DateTime.UtcNow,
+                    Guid.NewGuid()
+                ),
+                new User(
+                    Guid.NewGuid(),
+                    "iftekhar@gmail.com",
+                    "Iftekhar",
+                    "Jihad",
+                    DateTime.UtcNow,
+                    Guid.NewGuid()
+                ),
+            };
+            return Ok(users);
+        }
+    }
+}
